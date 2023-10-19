c5040abfd21277cfcc3b294c8400bde8caec0598
Chandra Penke <chandrapenke@gmail.com>
2023-10-19 14:22:15 -0700

    fix: unwind some changes

--- crates/ic-ingress-validator-util/src/lib.rs
+++ crates/ic-ingress-validator-util/src/lib.rs
@@ -1,15 +1,12 @@
+use ic_agent::agent::http_transport::ReqwestHttpReplicaV2Transport;
+use ic_agent::identity::AnonymousIdentity;
+use ic_agent::Agent;
 use ic_crypto_utils_threshold_sig_der::parse_threshold_sig_key_from_der;
+use ic_validator_ingress_message::IngressMessageVerifier;
 use instrumented_error::Result;
 use std::sync::Arc;
 
-pub use ic_interfaces::crypto::IngressSigVerifier;
-pub use ic_validator_ingress_message::IngressMessageVerifier;
-
 pub async fn try_new_ingress_verifier(url: &str) -> Result<IngressMessageVerifier> {
-    use ic_agent::agent::http_transport::ReqwestHttpReplicaV2Transport;
-    use ic_agent::identity::AnonymousIdentity;
-    use ic_agent::Agent;
-
     let agent: Agent = Agent::builder()
         .with_transport(ReqwestHttpReplicaV2Transport::create(url)?)
         .with_arc_identity(Arc::new(AnonymousIdentity))
